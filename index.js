405f18c680c8922a010f649c61fc1477da5111c9
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-07 15:00:31 -0500

    added display div toggling on button click

---
+++ index.js
@@ -0,0 +1,23 @@
+// DOM ELEMENTS
+
+const divOne = document.querySelector("#divOne")
+const divTwo = document.querySelector("#divTwo")
+const divThree = document.querySelector("#divThree")
+const btnDiv = document.querySelector("#btnDiv")
+
+// EVENT HANDLERS
+
+btnDiv.addEventListener("click", ({ target }) => {
+	if (target.matches("#btnOne")) {
+		[divTwo, divThree].forEach(div => div.classList.add("invisible"))
+		divOne.classList.remove("invisible")
+	} else if (target.matches("#btnTwo")) {
+		[divOne, divThree].forEach(div => div.classList.add("invisible"))
+		divTwo.classList.remove("invisible")
+	} else if (target.matches("#btnThree")) {
+		[divOne, divTwo].forEach(div => div.classList.add("invisible"))
+		divThree.classList.remove("invisible")
+	} else if (target.matches("#btnAll")) {
+		[divOne, divTwo, divThree].forEach(div => div.classList.remove("invisible"))
+	}
+})
